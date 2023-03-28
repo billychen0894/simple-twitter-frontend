5a31d2deda5b275b1bb2dca44ed49d76e8d5547b
Billy Chen <billychen0894@gmail.com>
2023-03-27 23:33:54 -0700

    add PrivateRoutes components to protect private routes

--- src/shared/pages/MainRoutes.jsx
+++ src/shared/pages/MainRoutes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import RootLayout from 'shared/pages/RootLayout';
 import TweetModal from 'users/components/TweetModal/TweetModal';
@@ -7,10 +7,11 @@ import TweetList from 'users/components/TweetList/TweetList';
 import UserTweetReply from 'users/pages/UserTweetReply';
 import UserTweet from 'users/pages/UserTweet';
 import { replyList, tweetsList, usersList } from 'constants/constants';
-import Setting from './Setting';
-import Login from './Login';
-import Register from './Register';
-import AdminLogin from './AdminLogin';
+import Setting from 'shared/pages/Setting';
+import Login from 'shared/pages/Login';
+import Register from 'shared/pages/Register';
+import AdminLogin from 'shared/pages/AdminLogin';
+import PrivateRoutes from 'shared/utils/PrivateRoutes';
 
 function MainRoutes({ location }) {
   const userInfo = usersList.find((user) => user.userId === 'u1');
@@ -35,64 +36,67 @@ function MainRoutes({ location }) {
       <Route path="login" element={<Login />} />
       <Route path="admin_login" element={<AdminLogin />} />
       <Route path="register" element={<Register />} />
-      <Route element={<RootLayout />}>
-        <Route path="setting" element={<Setting />} />
-        <Route path="home" element={<UserTweet />}>
-          <Route path="compose/tweet" element={<TweetModal />} />
-        </Route>
-        <Route path="/:userId/reply/:tweetId" element={<UserTweetReply />}>
-          <Route path="compose/tweet" element={<TweetModal />} />
-        </Route>
-        <Route path=":userId" element={<UserProfile />}>
-          <Route
-            path=""
-            element={
-              <TweetList
-                listType="homeTweets"
-                listItems={followingUsersTweetsList}
-              />
-            }
-          />
-          <Route
-            path="reply"
-            element={
-              <TweetList
-                listType="userReply"
-                listItems={tweetReplyList}
-                tweetUserName={userInfo.name}
-              />
-            }
-          />
+      <Route path="*" element={<Navigate to="login" />} />
+      <Route element={<PrivateRoutes />}>
+        <Route element={<RootLayout />}>
+          <Route path="setting" element={<Setting />} />
+          <Route path="home" element={<UserTweet />}>
+            <Route path="compose/tweet" element={<TweetModal />} />
+          </Route>
+          <Route path="/:userId/reply/:tweetId" element={<UserTweetReply />}>
+            <Route path="compose/tweet" element={<TweetModal />} />
+          </Route>
+          <Route path=":userId" element={<UserProfile />}>
+            <Route
+              path=""
+              element={
+                <TweetList
+                  listType="homeTweets"
+                  listItems={followingUsersTweetsList}
+                />
+              }
+            />
+            <Route
+              path="reply"
+              element={
+                <TweetList
+                  listType="userReply"
+                  listItems={tweetReplyList}
+                  tweetUserName={userInfo.name}
+                />
+              }
+            />
 
-          <Route
-            path="like"
-            element={
-              <TweetList
-                listType="homeTweets"
-                listItems={followingUsersTweetsList}
-              />
-            }
-          />
+            <Route
+              path="like"
+              element={
+                <TweetList
+                  listType="homeTweets"
+                  listItems={followingUsersTweetsList}
+                />
+              }
+            />
 
-          <Route
-            path="followers"
-            element={
-              <TweetList
-                listType="otherUsersTweets"
-                listItems={followersTweetsList}
-              />
-            }
-          />
+            <Route
+              path="followers"
+              element={
+                <TweetList
+                  listType="otherUsersTweets"
+                  listItems={followersTweetsList}
+                />
+              }
+            />
 
-          <Route
-            path="following"
-            element={
-              <TweetList
-                listType="otherUsersTweets"
-                listItems={followingTweetsList}
-              />
-            }
-          />
+            <Route
+              path="following"
+              element={
+                <TweetList
+                  listType="otherUsersTweets"
+                  listItems={followingTweetsList}
+                />
+              }
+            />
+          </Route>
         </Route>
       </Route>
     </Routes>
