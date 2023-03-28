3f10c3c0086059e8c2cc0be150205c501db64ed2
Billy Chen <billychen0894@gmail.com>
2023-03-27 23:32:58 -0700

    fix compile issue with webpack

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    buffer: require.resolve('buffer/'),
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    util: require.resolve('util/'),
+  };
+
+  return config;
+};
