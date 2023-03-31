fdfcf6f975f796f48d81fda7e0a5e7881fe55a9d
Billy Chen <billychen0894@gmail.com>
2023-03-31 02:32:16 -0700

    add utils

---
+++ src/api/utils.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+const sendRequest = async (
+  endpoint,
+  method = 'GET',
+  data = null,
+  headers = {}
+) => {
+  const authToken = localStorage.getItem('authToken');
+  if (!authToken) {
+    throw new Error('Error: No authorization token found.');
+  }
+
+  const url = `${apiUrl}${endpoint}`;
+  const axiosConfig = {
+    method,
+    url,
+    data,
+    headers: {
+      ...headers,
+      Authorization: `Bearer ${authToken}`,
+    },
+  };
+
+  try {
+    const response = await axios(axiosConfig);
+    return response.data;
+  } catch (error) {
+    throw new Error(`API request failed: ${error.message}`);
+  }
+};
+
+export default sendRequest;
