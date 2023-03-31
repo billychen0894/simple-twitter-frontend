4c6728d2211876479fcccfd08771aa894b5d35e8
Billy Chen <billychen0894@gmail.com>
2023-03-31 03:56:23 -0700

    update basename

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const basename = process.env.REACT_APP_PUBLIC_URL;
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
