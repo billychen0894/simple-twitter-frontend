75b61a478a3d11218e25fe0d0b03f56db581c531
Billy Chen <billychen0894@gmail.com>
2023-03-31 02:30:45 -0700

    add following items to fetch top 10 user followers

--- src/shared/components/Followers/FollowersList.jsx
+++ src/shared/components/Followers/FollowersList.jsx
@@ -1,8 +1,22 @@
 import Card from 'shared/components/UIElements/Card';
 import FollowersListItem from 'shared/components/Followers/FollowersListItem';
 import styles from 'shared/components/Followers/FollowersList.module.scss';
+import { useUsers } from 'contexts/UsersContext';
 
 function FollowersList() {
+  const { top10Users, currUserFollowList, deleteUserFollow, updateUserFollow } =
+    useUsers();
+
+  const handleToggleFollowing = (userId) => {
+    const isFollowing = currUserFollowList.some(
+      (user) => user.followingId === userId
+    );
+    if (isFollowing) {
+      return deleteUserFollow(userId);
+    }
+
+    return updateUserFollow({ id: userId });
+  };
   return (
     <div className={styles.followers}>
       <Card className={styles.followersListContainer}>
@@ -10,12 +24,27 @@ function FollowersList() {
         <hr />
         <div>
           <ul>
-            <FollowersListItem following />
-            <FollowersListItem following />
-            <FollowersListItem />
-            <FollowersListItem />
-            <FollowersListItem />
-            <FollowersListItem />
+            {top10Users
+              ? top10Users.map((item) => {
+                  return (
+                    <FollowersListItem
+                      key={item.id}
+                      userId={item.id}
+                      name={item.name}
+                      avatar={item.avatar}
+                      account={item.account}
+                      isFollowing={
+                        currUserFollowList
+                          ? currUserFollowList.some(
+                              (user) => user.followingId === item.id
+                            )
+                          : false
+                      }
+                      onToggleFollow={handleToggleFollowing}
+                    />
+                  );
+                })
+              : undefined}
           </ul>
         </div>
       </Card>
