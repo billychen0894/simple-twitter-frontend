8c979e45f30175f2243edc2b1a0e5765868ed1c8
Billy Chen <billychen0894@gmail.com>
2023-03-31 03:26:29 -0700

    update eslint

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const basename = process.env.PUBLIC_URL;
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
