29d266d8f5e971d6def93e22d8d1c3cbe51f65f6
Billy Chen <billychen0894@gmail.com>
2023-04-01 02:09:03 -0700

    add edit setting api

--- src/shared/pages/Setting.jsx
+++ src/shared/pages/Setting.jsx
@@ -1,4 +1,6 @@
-import useForm from 'hooks/useForm';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+
 import Button from 'shared/components/UIElements/Button';
 import Input from 'shared/components/UIElements/Input';
 import {
@@ -7,21 +9,25 @@ import {
   VALIDATOR_EMAIL,
   VALIDATOR_PASSWORD_VERIFY,
 } from 'shared/utils/validators';
+import useForm from 'hooks/useForm';
+import { useUsers } from 'contexts/UsersContext';
 import styles from 'shared/pages/Setting.module.scss';
 
-function Setting() {
+function Setting({ userData }) {
+  const { updateUserSettings } = useUsers();
+
   const initialFormInputs = {
     account: {
-      val: '',
-      isValid: false,
+      val: userData?.account || '',
+      isValid: userData?.account || false,
     },
     name: {
-      val: '',
-      isValid: false,
+      val: userData?.name || '',
+      isValid: userData?.name || false,
     },
     email: {
-      val: '',
-      isValid: false,
+      val: userData?.email || '',
+      isValid: userData?.email || false,
     },
     password: {
       val: '',
@@ -32,7 +38,38 @@ function Setting() {
       isValid: false,
     },
   };
+
   const [formState, handleInput] = useForm(initialFormInputs, false);
+  const [settingsError, setSettingsError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formState.formIsValid) {
+      return;
+    }
+
+    const payload = {
+      account: formState.inputs.account.val,
+      email: formState.inputs.email.val,
+      name: formState.inputs.name.val,
+      password: formState.inputs.password.val,
+      checkPassword: formState.inputs.passwordVerify.val,
+    };
+    const response = await updateUserSettings(payload);
+
+    const { success } = response;
+
+    if (!success) {
+      setSettingsError(true);
+      toast.error('編輯失敗');
+    }
+    if (success) {
+      setSettingsError(false);
+      toast.success('編輯成功');
+    }
+  };
+
   return (
     <div className={styles.settingContainer}>
       <header className={styles.settingHeader}>帳戶設定</header>
@@ -51,7 +88,12 @@ function Setting() {
                 ? '帳號不能超過16個字數'
                 : '請填寫帳號'
             }
-            inputStyles={styles.input}
+            inputStyles={`${styles.input} ${
+              settingsError ? styles.formInvalid : undefined
+            }`}
+            onInputChangeStyle={setSettingsError}
+            initialValue={initialFormInputs.account.val}
+            initialValid={initialFormInputs.account.isValid}
           />
           <Input
             id="name"
@@ -66,7 +108,12 @@ function Setting() {
                 ? '名稱不能超過12個字數'
                 : '請填寫名稱'
             }
-            inputStyles={styles.input}
+            inputStyles={`${styles.input} ${
+              settingsError ? styles.formInvalid : undefined
+            }`}
+            onInputChangeStyle={setSettingsError}
+            initialValue={initialFormInputs.name.val}
+            initialValid={initialFormInputs.name.isValid}
           />
           <Input
             id="email"
@@ -81,7 +128,12 @@ function Setting() {
                 ? '請輸入正確的Email格式'
                 : '請填寫Email'
             }
-            inputStyles={styles.input}
+            inputStyles={`${styles.input} ${
+              settingsError ? styles.formInvalid : undefined
+            }`}
+            onInputChangeStyle={setSettingsError}
+            initialValue={initialFormInputs.email.val}
+            initialValid={initialFormInputs.email.isValid}
           />
           <Input
             id="password"
@@ -96,7 +148,12 @@ function Setting() {
                 ? '密碼不能超過16個字數'
                 : '請輸入密碼'
             }
-            inputStyles={styles.input}
+            inputStyles={`${styles.input} ${
+              settingsError ? styles.formInvalid : undefined
+            }`}
+            onInputChangeStyle={setSettingsError}
+            initialValue={initialFormInputs.password.val}
+            initialValid={initialFormInputs.password.isValid}
           />
           <Input
             id="passwordVerify"
@@ -111,12 +168,18 @@ function Setting() {
             ]}
             onInput={handleInput}
             errorText="密碼不一致"
-            inputStyles={styles.input}
+            inputStyles={`${styles.input} ${
+              settingsError ? styles.formInvalid : undefined
+            }`}
+            onInputChangeStyle={setSettingsError}
+            initialValue={initialFormInputs.passwordVerify.val}
+            initialValid={initialFormInputs.passwordVerify.isValid}
           />
           <div className={styles.actionContainer}>
             <Button
               disabled={!formState.formIsValid}
               className={styles.formBtn}
+              onClick={handleSubmit}
             >
               儲存
             </Button>
