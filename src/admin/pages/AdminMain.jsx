e797d49b8c658900e7f784c33a1bca9aeb10e6ed
Billy Chen <billychen0894@gmail.com>
2023-04-02 02:49:41 -0700

    fix users list view from admin access

--- src/admin/pages/AdminMain.jsx
+++ src/admin/pages/AdminMain.jsx
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
+import { MoonLoader } from 'react-spinners';
 
 import AdminHeader from 'admin/AdminHeader/AdminHeader';
 import AdminTweetList from 'admin/AdminTweetList/AdminTweetList';
 import { useTweets } from 'contexts/TweetsContext';
-import { MoonLoader } from 'react-spinners';
+import { useAdmin } from 'contexts/AdminContext';
+import { toast } from 'react-toastify';
 import styles from 'admin/pages/AdminMain.module.scss';
 
 function AdminMain() {
-  const { tweets, fetchTweets, isLoading } = useTweets();
+  const { tweets, fetchTweets, isLoading, setTweets } = useTweets();
+  const { deleteUserTweet } = useAdmin();
 
   useEffect(() => {
     const authToken = localStorage.getItem('authToken');
@@ -17,10 +20,29 @@ function AdminMain() {
     }
   }, [fetchTweets, tweets.length, tweets]);
 
+  const handleDeleteUserTweet = async (id) => {
+    const response = await deleteUserTweet(id);
+
+    if (response.status === 'error') {
+      toast.error(response.message);
+      return;
+    }
+    toast.success('刪除成功!');
+    const updatedTweets = tweets.filter((tweet) => tweet.id !== id);
+    if (updatedTweets) {
+      setTweets(updatedTweets);
+    }
+  };
+
   return (
     <div className={styles.adminMainContainer}>
       <AdminHeader adminMain label="推文清單" />
-      {!isLoading ? <AdminTweetList listItems={tweets} /> : undefined}
+      {!isLoading ? (
+        <AdminTweetList
+          listItems={tweets}
+          onHandleDeleteUserTweet={handleDeleteUserTweet}
+        />
+      ) : undefined}
       <MoonLoader
         color="#FF974A"
         loading={isLoading}
--- src/admin/pages/AdminUserList.jsx
+++ src/admin/pages/AdminUserList.jsx
@@ -1,37 +1,36 @@
-import AdminNavigation from 'shared/components/Navigation/AdminNavigation';
+import { useEffect } from 'react';
+import { MoonLoader } from 'react-spinners';
+
 import AdminHeader from 'admin/AdminHeader/AdminHeader';
 import AdminUserCardList from 'admin/AdminUserCard/AdminUserCardList';
-import styles from 'admin/pages/AdminMain.module.scss';
-// import { getAdminUsers } from 'api/admin';
-// import { useEffect, useState } from 'react';
-// import { useAuth } from 'contexts/AuthContext';
-import { useEffect } from 'react';
-import { useUsers } from 'contexts/UsersContext';
+import { useAdmin } from 'contexts/AdminContext';
+import styles from 'admin/pages/AdminUserList.module.scss';
 
-function AdminUserList(cardItems) {
-  const { fetchAdminUsers, adminUsers } = useUsers();
+function AdminUserList() {
+  const { fetchAllUsers, isLoading, usersList } = useAdmin();
 
   useEffect(() => {
     const authToken = localStorage.getItem('authToken');
 
-    if (authToken && adminUsers.length === 0) {
-      fetchAdminUsers();
+    if (authToken && usersList.length === 0) {
+      fetchAllUsers();
     }
-  }, [fetchAdminUsers, adminUsers.length]);
+  }, [fetchAllUsers, usersList.length]);
 
   return (
-    <div className={styles.adminUserContainer}>
-      <div className={styles.container}>
-        <AdminNavigation className={styles.navigation} />
-        <div className={styles.main}>
-          <AdminHeader
-            adminUser
-            label="使用者清單"
-            className={styles.AdminHeader}
-          />
-          <AdminUserCardList cardItems={cardItems} className={styles.list} />
-        </div>
-      </div>
+    <div className={styles.adminMainContainer}>
+      <AdminHeader adminUser label="使用者列表" />
+      {!isLoading ? <AdminUserCardList cardItems={usersList} /> : undefined}
+      <MoonLoader
+        color="#FF974A"
+        loading={isLoading}
+        speedMultiplier={1}
+        cssOverride={{
+          margin: '0 auto',
+          position: 'relative',
+          top: '25%',
+        }}
+      />
     </div>
   );
 }
